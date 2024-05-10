d993835a1e1e7268e9180a9db9130f4097084a40
jjamva <hongi97@naver.com>
2024-05-10 20:49:49 +0900

    catch all routing

--- app/archive/@latest/default.js
+++ app/archive/@latest/default.js
@@ -1,4 +1,13 @@
+import NewsList from "@/components/news-list";
+import { getLatestNews } from "@/lib/news";
+
 //default.js 병렬 라우팅을 다룰 때, 사용
 export default function LatestNewsPage() {
-  return <h1>LatestNews 페이지</h1>;
+  const latestNews = getLatestNews();
+  return (
+    <>
+      <h2>LatestNews 페이지</h2>
+      <NewsList news={latestNews} />
+    </>
+  );
 }
